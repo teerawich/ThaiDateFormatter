5f579227fe56b1eb9130d9e8e2099039607c625a
Teerawich <teerawich@giinos.com>
2025-01-23 20:44:21 +0700

    [add] example for library.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
-## 0.0.1
+# Changelog
 
-* TODO: Describe initial release.
+## 1.0.0
+### Added
+- Initial release of `ThaiDateFormatter` library.
+
+### Fixed
+- N/A (First release, no fixes yet)
---
+++ example/main.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:thai_date_formatter/thai_date_formatter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Thai Date Formatter'),
+        ),
+        body: Center(
+          child: Column(
+            spacing: 10.0,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(text: 'Now: '),
+                    TextSpan(text: '${DateTime.now()}', style: TextStyle(fontWeight: FontWeight.bold)),
+                  ]
+                ),
+              ),
+              SizedBox(height: 10.0),
+              RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(text: 'Full month and full year: '),
+                      TextSpan(
+                          text: ThaiDateFormatter.format(
+                          DateTime.now(),
+                          ThaiDateFormatType.fullMonthFullYear
+                          ), style: TextStyle(fontWeight: FontWeight.bold)
+                      ),
+                    ]
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(text: 'Full month and short year: '),
+                      TextSpan(
+                          text: ThaiDateFormatter.format(
+                              DateTime.now(),
+                              ThaiDateFormatType.fullMonthShortYear
+                          ), style: TextStyle(fontWeight: FontWeight.bold)
+                      ),
+                    ]
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(text: 'Short month and full year: '),
+                      TextSpan(
+                          text: ThaiDateFormatter.format(
+                              DateTime.now(),
+                              ThaiDateFormatType.shortMonthFullYear
+                          ), style: TextStyle(fontWeight: FontWeight.bold)
+                      ),
+                    ]
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(text: 'Short month and short year: '),
+                      TextSpan(
+                          text: ThaiDateFormatter.format(
+                              DateTime.now(),
+                              ThaiDateFormatType.shortMonthShortYear
+                          ), style: TextStyle(fontWeight: FontWeight.bold)
+                      ),
+                    ]
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(text: 'Short month, short year and show day of week (short): '),
+                      TextSpan(
+                          text: ThaiDateFormatter.format(
+                            DateTime.now(),
+                            ThaiDateFormatType.shortMonthShortYear,
+                            showDayOfWeek: true,
+                            shortDayOfWeek: true,
+                          ), style: TextStyle(fontWeight: FontWeight.bold)
+                      ),
+                    ]
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(text: 'Short month, short year and show day of week: '),
+                      TextSpan(
+                          text: ThaiDateFormatter.format(
+                            DateTime.now(),
+                            ThaiDateFormatType.shortMonthShortYear,
+                            showDayOfWeek: true,
+                            shortDayOfWeek: false,
+                          ), style: TextStyle(fontWeight: FontWeight.bold)
+                      ),
+                    ]
+                ),
+              ),
+              SizedBox(height: 10.0),
+              RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(text: 'Specific date 2025-01-01 (none padding) : '),
+                      TextSpan(
+                          text: ThaiDateFormatter.format(
+                            DateTime.parse('2025-01-01'),
+                            ThaiDateFormatType.fullMonthFullYear,
+                          ), style: TextStyle(fontWeight: FontWeight.bold)
+                      ),
+                    ]
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(text: 'Specific date 2025-01-01 (with padding) : '),
+                      TextSpan(
+                          text: ThaiDateFormatter.format(
+                            DateTime.parse('2025-01-01'),
+                            ThaiDateFormatType.fullMonthFullYear,
+                            padDayWithZero: true
+                          ), style: TextStyle(fontWeight: FontWeight.bold)
+                      ),
+                    ]
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+
+  }
+
+}
