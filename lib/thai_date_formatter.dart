09ed28ee6f1b0c81ac08b1b188a4df577feba8e3
Teerawich <teerawich@giinos.com>
2025-01-23 19:02:23 +0700

    [update] add unit test and complete function.

--- lib/thai_date_formatter.dart
+++ lib/thai_date_formatter.dart
@@ -1,5 +1,139 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+enum ThaiDateFormatType {
+  fullMonthFullYear,
+  fullMonthShortYear,
+  shortMonthFullYear,
+  shortMonthShortYear,
+}
+
+class ThaiDateFormatter {
+  static const Map<int, String> _thaiMonthsFull = {
+    1: 'มกราคม',
+    2: 'กุมภาพันธ์',
+    3: 'มีนาคม',
+    4: 'เมษายน',
+    5: 'พฤษภาคม',
+    6: 'มิถุนายน',
+    7: 'กรกฎาคม',
+    8: 'สิงหาคม',
+    9: 'กันยายน',
+    10: 'ตุลาคม',
+    11: 'พฤศจิกายน',
+    12: 'ธันวาคม',
+  };
+
+  static const Map<int, String> _thaiMonthsShort = {
+    1: 'ม.ค.',
+    2: 'ก.พ.',
+    3: 'มี.ค.',
+    4: 'เม.ย.',
+    5: 'พ.ค.',
+    6: 'มิ.ย.',
+    7: 'ก.ค.',
+    8: 'ส.ค.',
+    9: 'ก.ย.',
+    10: 'ต.ค.',
+    11: 'พ.ย.',
+    12: 'ธ.ค.',
+  };
+
+  static const Map<int, String> _thaiDaysFull = {
+    1: 'จันทร์',
+    2: 'อังคาร',
+    3: 'พุธ',
+    4: 'พฤหัสบดี',
+    5: 'ศุกร์',
+    6: 'เสาร์',
+    7: 'อาทิตย์',
+  };
+
+  static const Map<int, String> _thaiDaysShort = {
+    1: 'จ.',
+    2: 'อ.',
+    3: 'พ.',
+    4: 'พฤ.',
+    5: 'ศ.',
+    6: 'ส.',
+    7: 'อา.',
+  };
+
+  static String format(
+    DateTime date,
+    ThaiDateFormatType formatType, {
+    bool useThaiNumbers = false,
+    bool padDayWithZero = false,
+    bool showDayOfWeek = false,
+    bool shortDayOfWeek = false,
+  }) {
+    final day = padDayWithZero
+        ? date.day.toString().padLeft(2, '0')
+        : date.day.toString();
+    final int month = date.month;
+    final int year = date.year + 543;
+    final int weekday = date.weekday;
+
+    /// Set day of week
+    String dayOfWeek = '';
+    if (showDayOfWeek) {
+      dayOfWeek =
+          shortDayOfWeek ? _thaiDaysShort[weekday]! : _thaiDaysFull[weekday]!;
+      dayOfWeek += ' ';
+    }
+
+    String result;
+    switch (formatType) {
+      case ThaiDateFormatType.fullMonthFullYear:
+        result = '$day ${_thaiMonthsFull[month]} $year';
+        break;
+      case ThaiDateFormatType.shortMonthFullYear:
+        result = '$day ${_thaiMonthsShort[month]} $year';
+        break;
+      case ThaiDateFormatType.fullMonthShortYear:
+        result =
+            '$day ${_thaiMonthsFull[month]} ${year.toString().substring(2)}';
+        break;
+      case ThaiDateFormatType.shortMonthShortYear:
+        result =
+            '$day ${_thaiMonthsShort[month]} ${year.toString().substring(2)}';
+        break;
+    }
+
+    result = dayOfWeek + result;
+
+    return useThaiNumbers ? ThaiNumberFormatter.toThaiNumbers(result) : result;
+  }
+}
+
+
+/// Utility class for converting Arabic numerals to Thai numerals.
+class ThaiNumberFormatter {
+  static const Map<String, String> _thaiNumbers = {
+    '0': '๐',
+    '1': '๑',
+    '2': '๒',
+    '3': '๓',
+    '4': '๔',
+    '5': '๕',
+    '6': '๖',
+    '7': '๗',
+    '8': '๘',
+    '9': '๙',
+  };
+
+  /// Converts Arabic numerals in the input string to Thai numerals.
+  static String toThaiNumbers(String input) {
+
+    /// handle empty input
+    if (input.isEmpty) return input;
+
+    final buffer = StringBuffer();
+    for (final char in input.runes) {
+      /// char is unicode from input.runes
+      final thaiChar = _thaiNumbers[String.fromCharCode(char)];
+
+      /// if thaiChar cannot mapping return old character.
+      buffer.write(thaiChar ?? String.fromCharCode(char));
+    }
+
+    return buffer.toString();
+  }
 }
--- test/thai_date_formatter_test.dart
+++ test/thai_date_formatter_test.dart
@@ -3,10 +3,147 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:thai_date_formatter/thai_date_formatter.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('ThaiDateFormatter',() {
+
+    test('Format full month and full year',() {
+      final date = DateTime.parse("2025-01-01");
+      final result = ThaiDateFormatter.format(
+          date,
+          ThaiDateFormatType.fullMonthFullYear
+      );
+
+      expect(result, '1 มกราคม 2568');
+
+      final dateWithTime = DateTime.parse('2025-01-02 12:00:00');
+      final resultDateWithTime = ThaiDateFormatter.format(
+        dateWithTime,
+        ThaiDateFormatType.fullMonthFullYear,
+        padDayWithZero: true,
+      );
+
+      expect(resultDateWithTime, '02 มกราคม 2568');
+
+      final dateWithTimeUseThaiFormat = DateTime.parse('2025-01-02 12:00:00');
+      final resultDateWithTimeUseThaiFormat = ThaiDateFormatter.format(
+        dateWithTimeUseThaiFormat,
+        ThaiDateFormatType.fullMonthFullYear,
+        useThaiNumbers: true,
+        padDayWithZero: true,
+      );
+
+      expect(resultDateWithTimeUseThaiFormat, '๐๒ มกราคม ๒๕๖๘');
+
+      final dateWithTimeUseThaiFormatAndWeekday = DateTime.parse('2025-01-02 12:00:00');
+      final resultDateWithTimeUseThaiFormatAndWeekday = ThaiDateFormatter.format(
+        dateWithTimeUseThaiFormatAndWeekday,
+        ThaiDateFormatType.fullMonthFullYear,
+        useThaiNumbers: true,
+        padDayWithZero: true,
+        showDayOfWeek: true,
+      );
+
+      expect(resultDateWithTimeUseThaiFormatAndWeekday, 'พฤหัสบดี ๐๒ มกราคม ๒๕๖๘');
+
+      final dateWithTimeUseThaiFormatAndWeekdayShortFormat = DateTime.parse('2025-01-02 12:00:00');
+      final resultDateWithTimeUseThaiFormatAndWeekdayShortFormat = ThaiDateFormatter.format(
+        dateWithTimeUseThaiFormatAndWeekdayShortFormat,
+        ThaiDateFormatType.fullMonthFullYear,
+        useThaiNumbers: true,
+        padDayWithZero: true,
+        showDayOfWeek: true,
+        shortDayOfWeek: true,
+      );
+
+      expect(resultDateWithTimeUseThaiFormatAndWeekdayShortFormat, 'พฤ. ๐๒ มกราคม ๒๕๖๘');
+    });
+
+    test('Format short month and full year', () {
+      final date = DateTime.parse("2025-01-01");
+      final result = ThaiDateFormatter.format(
+          date,
+          ThaiDateFormatType.shortMonthFullYear
+      );
+
+      expect(result, '1 ม.ค. 2568');
+
+      final dateWithTime = DateTime.parse('2025-01-02 12:00:00');
+      final resultDateWithTime = ThaiDateFormatter.format(
+        dateWithTime,
+        ThaiDateFormatType.shortMonthFullYear,
+        padDayWithZero: true,
+      );
+
+      expect(resultDateWithTime, '02 ม.ค. 2568');
+
+      final dateWithTimeUseThaiFormat = DateTime.parse('2025-01-02 12:00:00');
+      final resultDateWithTimeUseThaiFormat = ThaiDateFormatter.format(
+        dateWithTimeUseThaiFormat,
+        ThaiDateFormatType.shortMonthFullYear,
+        useThaiNumbers: true,
+        padDayWithZero: true,
+      );
+
+      expect(resultDateWithTimeUseThaiFormat, '๐๒ ม.ค. ๒๕๖๘');
+    });
+
+    test('Format full month and short year', () {
+      final date = DateTime.parse("2025-01-01");
+      final result = ThaiDateFormatter.format(
+          date,
+          ThaiDateFormatType.fullMonthShortYear
+      );
+
+      expect(result, '1 มกราคม 68');
+
+      final dateWithTime = DateTime.parse('2025-01-02 12:00:00');
+      final resultDateWithTime = ThaiDateFormatter.format(
+        dateWithTime,
+        ThaiDateFormatType.fullMonthShortYear,
+        padDayWithZero: true,
+      );
+
+      expect(resultDateWithTime, '02 มกราคม 68');
+
+      final dateWithTimeUseThaiFormat = DateTime.parse('2025-01-02 12:00:00');
+      final resultDateWithTimeUseThaiFormat = ThaiDateFormatter.format(
+        dateWithTimeUseThaiFormat,
+        ThaiDateFormatType.fullMonthShortYear,
+        useThaiNumbers: true,
+        padDayWithZero: true,
+      );
+
+      expect(resultDateWithTimeUseThaiFormat, '๐๒ มกราคม ๖๘');
+    });
+
+    test('Format short month and short year', () {
+      final date = DateTime.parse("2025-01-01");
+      final result = ThaiDateFormatter.format(
+          date,
+          ThaiDateFormatType.shortMonthShortYear
+      );
+
+      expect(result, '1 ม.ค. 68');
+
+      final dateWithTime = DateTime.parse('2025-01-02 12:00:00');
+      final resultDateWithTime = ThaiDateFormatter.format(
+        dateWithTime,
+        ThaiDateFormatType.shortMonthShortYear,
+        padDayWithZero: true,
+      );
+
+      expect(resultDateWithTime, '02 ม.ค. 68');
+
+      final dateWithTimeUseThaiFormat = DateTime.parse('2025-01-02 12:00:00');
+      final resultDateWithTimeUseThaiFormat = ThaiDateFormatter.format(
+        dateWithTimeUseThaiFormat,
+        ThaiDateFormatType.shortMonthShortYear,
+        useThaiNumbers: true,
+        padDayWithZero: true,
+      );
+
+      expect(resultDateWithTimeUseThaiFormat, '๐๒ ม.ค. ๖๘');
+    });
+
   });
+
 }
